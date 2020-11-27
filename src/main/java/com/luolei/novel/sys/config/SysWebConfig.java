1584122ad817c56ba2918b1328c063c5dcea700a
luolei <920151816@qq.com>
2020-11-27 22:01:14 +0800

    test

--- src/main/java/com/luolei/novel/sys/config/SysWebConfig.java
+++ src/main/java/com/luolei/novel/sys/config/SysWebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class SysWebConfig implements WebMvcConfigurer {
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new TokenInterceptor())
